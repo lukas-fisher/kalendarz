2ffc003c4d43b8dae3660c1908f6c2b061ec5716
LF <76662147+lukas-fisher@users.noreply.github.com>
2021-01-17 22:52:01 +0100

    Update index.php
    ogólna drukarka w stylu klasycznego kalendarza. Działa. same dni zkońca łańcucha pokarmowego są obiektami.

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
 
-echo "mój kalnedarz<br/>";
-
-$wybrany_miesiac = "12";
+$wybrany_miesiac = "5";
 $wybrany_rok = "2021";
 
 $startowa = new DateTime($wybrany_rok."-".$wybrany_miesiac."-1");
@@ -30,6 +28,20 @@ for ($i=1; $i <= $roznica_int; $i++)
    $moj_miesiac[$tydzien][$dzien_tygodnia] = $data;
  }
 
-
+foreach ($moj_miesiac as $klucze => $wartosci) {
+  echo "W".$klucze." ";
+  for ($i = 1; $i<= 7; $i++)
+   {
+    if (isset($wartosci[$i]))
+     {
+        echo " [ ".$wartosci[$i]->format('d')." ] ";
+     }
+    else
+     {
+       echo " [ 00 ] " ;
+     }
+   }
+  echo "<br/>";
+}
 
 ?>
